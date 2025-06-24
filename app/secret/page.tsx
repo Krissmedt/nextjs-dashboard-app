3645bed6148076fb66a097a75dbbc702c4396c77
krissmedt <kris.smedt@lorezen.com>
2025-06-24 15:55:21 +0100

    Finish Chapter 7

--- app/secret/page.tsx
+++ app/secret/page.tsx
@@ -2,6 +2,6 @@ import {lusitana} from "@/app/ui/fonts";
 
 export default function Page() {
     return <div className="flex m-11 h-screen items-center justify-center bg-pink-300 shadow-lg shadow-indigo-500/50">
-        <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>Lorna you will be the best mum and I love you</p>
+        <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal p-6 bg-white/30 rounded-lg`}>Lorna you will be the best mum and I love you</p>
     </div>;
 }
